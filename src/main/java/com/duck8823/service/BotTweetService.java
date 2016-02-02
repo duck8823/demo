fc612cfa5475b7bde101090efa3bb92c2e3bce4a
maeda <duck8823@gmail.com>
2016-02-03 04:11:28 +0900

    お気に入り調整

--- src/main/java/com/duck8823/service/BotTweetService.java
+++ src/main/java/com/duck8823/service/BotTweetService.java
@@ -61,9 +61,9 @@ public class BotTweetService {
 
 	public void favorite() throws TwitterException {
 		int cnt = 0;
-		Filters filters = new Filters(filterRepository.list());
+		Filters filters = filterRepository.list();
 		for(Status status : twitter.search(QueryFactory.create("水族館")).getTweets()){
-			if(!containsPhoto(status.getMediaEntities()) || filter(filters, status) || status.getUser().getScreenName().equals(twitter.getScreenName()) || twitter.showStatus(status.getId()).isFavorited()){
+			if(!containsPhoto(status.getMediaEntities()) || filter(filters, status) || status.getUser().getId() == twitter.getId() || twitter.showStatus(status.getId()).isFavorited()){
 				continue;
 			}
 			try {
