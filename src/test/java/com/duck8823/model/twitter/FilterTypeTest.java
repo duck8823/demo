d8d46727fcb89fa815d791026b281a4f742dec7f
maeda <duck8823@gmail.com>
2016-02-16 23:11:01 +0900

    リファクタリング

--- src/test/java/com/duck8823/model/twitter/FilterTypeTest.java
+++ src/test/java/com/duck8823/model/twitter/FilterTypeTest.java
@@ -1,16 +1,13 @@
 package com.duck8823.model.twitter;
 
-import com.duck8823.TestConfiguration;
-import com.duck8823.context.twitter.QueryFactory;
 import com.duck8823.context.twitter.TestStatus;
 import com.duck8823.context.twitter.TestUser;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import twitter4j.*;
+import twitter4j.Status;
+import twitter4j.User;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -20,7 +17,6 @@ import static org.junit.Assert.assertTrue;
  */
 @Log4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfiguration.class)
 public class FilterTypeTest {
 
 	@Test
