3f1c818036469253eb5bfb99e56168457a9b08f5
maeda <duck8823@gmail.com>
2016-07-30 01:34:57 +0900

    RestControllerにした（CSRFで403）

--- src/main/java/com/duck8823/web/line/LineController.java
+++ src/main/java/com/duck8823/web/line/LineController.java
@@ -6,10 +6,12 @@ import com.linecorp.bot.model.callback.Event;
 import com.linecorp.bot.model.content.Content;
 import com.linecorp.bot.model.content.TextContent;
 import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,8 +19,9 @@ import java.util.List;
  * LINE BOT をためす
  * Created by maeda on 7/30/2016.
  */
+@Log4j
 @RequestMapping("line")
-@Controller
+@RestController
 public class LineController {
 
 	@Autowired
@@ -30,6 +33,7 @@ public class LineController {
 			Content content = event.getContent();
 			if (content instanceof TextContent) {
 				TextContent text = (TextContent) content;
+				log.debug(text.getText());
 				lineBotClient.sendText(text.getText(), text.getFrom());
 			}
 		}
