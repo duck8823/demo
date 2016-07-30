ed6a99125a1368410677904d9cebf612541036a9
maeda <duck8823@gmail.com>
2016-07-30 10:03:57 +0900

    Transactional漏れ

--- src/main/java/com/duck8823/web/line/LineController.java
+++ src/main/java/com/duck8823/web/line/LineController.java
@@ -3,30 +3,27 @@ package com.duck8823.web.line;
 import com.duck8823.model.bot.Talk;
 import com.duck8823.model.bot.TalkBot;
 import com.duck8823.model.photo.Photo;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.http.json.JsonHttpContent;
+import com.duck8823.service.PhotoService;
 import com.linecorp.bot.client.LineBotClient;
 import com.linecorp.bot.client.exception.LineBotAPIException;
 import com.linecorp.bot.model.callback.Event;
+import com.linecorp.bot.model.content.AddedAsFriendOperation;
 import com.linecorp.bot.model.content.Content;
 import com.linecorp.bot.model.content.TextContent;
 import com.linecorp.bot.spring.boot.annotation.LineBotMessages;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
-
-import static com.duck8823.model.photo.Place_.url;
 
 /**
  * LINE BOT をためす
  * Created by maeda on 7/30/2016.
  */
+@Transactional
 @SessionAttributes({"talk"})
 @Log4j
 @RequestMapping("line")
@@ -38,6 +35,9 @@ public class LineController {
 
 	@Autowired
 	private TalkBot talkBot;
+
+	@Autowired
+	private PhotoService photoService;
 
 	@ModelAttribute
 	private Talk talk() {
@@ -51,9 +51,19 @@ public class LineController {
 			if (content instanceof TextContent) {
 				TextContent text = (TextContent) content;
 
-				Talk response = talkBot.talk(talk.respond(text.getText()));
+				if (text.getText().contains("写真")){
+					Photo photo = photoService.random().get();
+					String url = "https://www.duck8823.com/photo/" + photo.getId();
+					lineBotClient.sendImage(text.getFrom(), url, url);
+				} else {
+					Talk response = talkBot.talk(talk.respond(text.getText()));
 
-				lineBotClient.sendText(text.getFrom(), response.getContent());
+					lineBotClient.sendText(text.getFrom(), response.getContent());
+				}
+			} else if (content instanceof AddedAsFriendOperation) {
+				AddedAsFriendOperation operation = (AddedAsFriendOperation) content;
+
+				lineBotClient.sendText(operation.getMid(), "このBotは docomo Developer supportのAPIを利用しています.\n会話の内容はドコモのサーバに送信されます.");
 			}
 		}
 	}
