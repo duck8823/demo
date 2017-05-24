2bbc6fbc356b86e23217acda8c30478c8124d605
shunsuke maeda <duck8823@gmail.com>
2017-05-25 00:50:34 +0900

    Update spring-boot

--- src/main/java/com/duck8823/ErrorConfiguration.java
+++ src/main/java/com/duck8823/ErrorConfiguration.java
@@ -1,7 +1,7 @@
 package com.duck8823;
 
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
